72100644f225b225514c21ce37838f895836842e
taeyeon0319 <xodus1349@likelion.org>
2025-07-25 13:50:46 +0900

    feat: 스플래시 적용 및 기본 런처 스플래시 미적용

--- app/src/main/java/com/motgolla/ui/navigation/MotgollaNavHost.kt
+++ app/src/main/java/com/motgolla/ui/navigation/MotgollaNavHost.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.motgolla.ui.screen.SplashScreen
 import com.motgolla.ui.screen.home.HomeScreen
 import com.motgolla.ui.screen.my.MyScreen
 import com.motgolla.ui.screen.record.RecordScreen
@@ -14,12 +15,16 @@ import com.motgolla.ui.screen.vote.VoteScreen
 fun MotgollaNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
     NavHost(
         navController = navController,
-        startDestination = "home",
+        startDestination = "splash",
         modifier = modifier
     ) {
+        composable("splash") {
+            SplashScreen(navController)
+        }
         composable("home") { HomeScreen() }
         composable("record") { RecordScreen() }
         composable("vote") { VoteScreen() }
         composable("my") { MyScreen() }
     }
 }
+
