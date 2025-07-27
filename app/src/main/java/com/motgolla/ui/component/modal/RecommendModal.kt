ad6b64ed3f6a54e19053c88c7a698d28d5678a7c
HideOnCodec <tlsdmsgp33@naver.com>
2025-07-27 22:26:33 +0900

    feat: 추천 아이템 미리보기 화면 구현

---
+++ app/src/main/java/com/motgolla/ui/component/modal/RecommendModal.kt
@@ -0,0 +1,15 @@
+package com.motgolla.ui.component.modal
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RecommendModal(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RecommendModalPreview() {
+    RecommendModal()
+}
