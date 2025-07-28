35351c3c343434da87a6c6f4fd69ac0ae68c02c9
HideOnCodec <tlsdmsgp33@naver.com>
2025-07-28 11:30:00 +0900

    fix: 가입일 고정 수정

--- app/src/main/java/com/motgolla/ui/screen/my/MyScreen.kt
+++ app/src/main/java/com/motgolla/ui/screen/my/MyScreen.kt
@@ -38,11 +38,11 @@ fun MyScreen(navController: NavController) {
     val context = LocalContext.current
 
     // 상태로 값 저장
-    var showModal by remember { mutableStateOf(false) }
     var nickname by remember { mutableStateOf("닉네임") }
     var profile by remember { mutableStateOf("https://via.placeholder.com/60") }
     var birthday by remember { mutableStateOf("생일 정보 없음") }
     var gender by remember { mutableStateOf("") }
+    var createdAt by remember { mutableStateOf("2025-06-02") }
 
     var checked by remember {
         mutableStateOf(
@@ -64,6 +64,7 @@ fun MyScreen(navController: NavController) {
                     profile = memberInfo.profile?.takeIf { it.isNotBlank() } ?: "https://via.placeholder.com/60"
                     birthday = memberInfo.birthday
                     gender = memberInfo.gender
+                    createdAt = memberInfo.createdAt.toString().take(10)
                 }
             } else {
                 println("회원 정보 조회 실패: ${response.code()}")
@@ -106,7 +107,7 @@ fun MyScreen(navController: NavController) {
             Spacer(modifier = Modifier.width(16.dp))
             Column {
                 Text(text = nickname, color = Color.White, fontSize = 25.sp, fontWeight = FontWeight.Bold)
-                Text(text = "2025.06.02 가입", color = Color.White, fontSize = 15.sp)
+                Text(text = "${createdAt} 가입", color = Color.White, fontSize = 15.sp)
             }
         }
 
