49a58f1bcec46939dcc4e6f58407c66cf7e2ac66
yeon2lee <md04254@naver.com>
2025-07-29 16:26:19 +0900

    feat: Title 글꼴 추가

--- app/src/main/java/com/motgolla/ui/theme/Typograpy.kt
+++ app/src/main/java/com/motgolla/ui/theme/Typograpy.kt
@@ -13,100 +13,104 @@ val Pretendard = FontFamily(
     Font(R.font.pretendard_bold, FontWeight.Bold)
 )
 
-val HappinessSans = FontFamily(
-    Font(R.font.happiness_sans_title, FontWeight.Normal),
-    Font(R.font.happiness_sans_bold, FontWeight.Bold),
-    Font(R.font.happiness_sans_regular, FontWeight.ExtraBold),
+val Paperlogy = FontFamily(
+    Font(R.font.paperlogy_medium, FontWeight.Medium),
+    Font(R.font.paperlogy_regular, FontWeight.Normal),
+    Font(R.font.paperlogy_semi_bold, FontWeight.SemiBold),
+    Font(R.font.paperlogy_bold, FontWeight.Bold),
+    Font(R.font.paperlogy_extra_bold, FontWeight.ExtraBold),
 )
 
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = HappinessSans,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Paperlogy,
+        fontWeight = FontWeight.ExtraBold,
         fontSize = 57.sp,
         lineHeight = 64.sp,
         letterSpacing = (-0.25).sp
     ),
     displayMedium = TextStyle(
-        fontFamily = HappinessSans,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Paperlogy,
+        fontWeight = FontWeight.Bold,
         fontSize = 45.sp,
         lineHeight = 52.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = HappinessSans,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Paperlogy,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 36.sp,
         lineHeight = 44.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = HappinessSans,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Paperlogy,
+        fontWeight = FontWeight.Bold,
         fontSize = 32.sp,
         lineHeight = 40.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = HappinessSans,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Paperlogy,
+        fontWeight = FontWeight.Medium,
         fontSize = 28.sp,
         lineHeight = 36.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = HappinessSans,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Paperlogy,
+        fontWeight = FontWeight.Medium,
         fontSize = 24.sp,
         lineHeight = 32.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = HappinessSans,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Paperlogy,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
         lineHeight = 28.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = HappinessSans,
+        fontFamily = Paperlogy,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = HappinessSans,
+        fontFamily = Paperlogy,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp
     ),
+
+    // 본문 영역: Pretendard 사용
     bodyLarge = TextStyle(
-        fontFamily = HappinessSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = HappinessSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = HappinessSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = HappinessSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = HappinessSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = HappinessSans,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp
