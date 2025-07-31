b2efedc1c1fad2abf28ae31befd1f806058f6017
yeon2lee <md04254@naver.com>
2025-07-31 09:16:53 +0900

    feat: 메인 색상 변경

--- app/src/main/java/com/motgolla/ui/theme/Color.kt
+++ app/src/main/java/com/motgolla/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.motgolla.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val MotgollaPrimary = Color(0xFF603DE3) // 보라색
+val MotgollaPrimary = Color(0xFF4d2af4) // 보라색
 val MotgollaSecondary = Color(0xFFFFCA76) // 노란색
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
